0a32141fcbf7052542d78fc4a6ecfc53b77604e5
farohiddin_M <farohiddinmamarasulov@gmail.com>
2023-02-20 23:27:08 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+//components 
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Instagram } from '@mui/icons-material'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+        <h1>
+          <Instagram style={{color:'orange',fontSize:40}} />
+        </h1>
+    </div>
+  )
+}
+
+export default Home
